c87cc4e4c48706910917bddaa6258f09524cf521
lebefriedlich <noval.akbar.906@gmail.com>
2024-12-06 12:41:01 +0700

    update again & again

--- resources/views/hasil-normalisasi-bobot.blade.php
+++ resources/views/hasil-normalisasi-bobot.blade.php
@@ -17,7 +17,7 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Hasil Normalisasi</h4>
+                            <h4 class="card-title">Hasil Normalisasi Bobot & Ranking</h4>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
